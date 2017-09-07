4df35eb94d72ecb96ebb2fc14989877a5a8649ea
totowen <136248471@qq.com>
2017-09-07 13:58:27 +0800

    修改配置 修改moudle

--- web/src/main/java/com/tos/TempleteApplication.java
+++ web/src/main/java/com/tos/TempleteApplication.java
@@ -2,6 +2,7 @@ package com.tos;
 
 import com.tos.StateMachineConfig.Events;
 import com.tos.StateMachineConfig.States;
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableAdminServer
 @RestController
 @SpringBootApplication
 //@EnableCaching
